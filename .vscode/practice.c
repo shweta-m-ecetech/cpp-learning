5f9eddd01af20f32898b52f3e1b878a08cdd0c4b
shweta-m-ecetech <shweta.m.ecetech@gmail.com>
2024-10-26 13:45:52 +0530

    Update practice.c

--- .vscode/practice.c
+++ .vscode/practice.c
@@ -3,3 +3,11 @@
 #include<math.h>
 int main()
 {
+    int n,arr[n],i;
+    printf("Enter the nom of elements");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        printf("Enter the %d element",i);
+        scanf("%d",&arr[i]
+    }
