e8f6cc6458cf46ffa39e1d103ef8bef8494cff1c
shweta-m-ecetech <shweta.m.ecetech@gmail.com>
2025-10-29 12:20:36 +0530

    Update array_q.py

--- array_q.py
+++ array_q.py
@@ -4,8 +4,14 @@ for i in range(len(a)):
      for j in range(len(a)-i):
          c=a[i:j]
          b.append(c)
+words=list()
 for string in a:
     for letter in string:
-        
+        e=list(string)
+        if e.count(letter)==1:
+              words.append(string)
+        else:
+              pass
 
     
+words=set(words)
