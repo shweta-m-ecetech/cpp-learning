0d5e765b44af67d7bee38761c7af460321a4badc
shweta-m-ecetech <shweta.m.ecetech@gmail.com>
2026-07-08 06:55:33 +0530

    Update 8_7_q1.c

--- 8_7_q1.c
+++ 8_7_q1.c
@@ -14,7 +14,7 @@ void main()
 
     for(int i = 0; i < 3; i++)
     {
-        for(int j = 0; j < 3; j++)
+        for(int j = i+1; j < 3; j++)
         {
             if(a[i][j] > a[i][j+1])
             {
@@ -27,7 +27,7 @@ void main()
 
     for(int i = 0; i < 3; i++)
     {
-        for(int j = 0; j < 3; j++)
+        for(int j = i+1; j < 3; j++)
         {
             if(a[i][j] > a[i+1][j])
             {
@@ -38,7 +38,7 @@ void main()
         }
     }
 
-    for(int i = 0; i < 3; i++)
+    for(int i = 0; i < 2; i++)
     {
         if(a[i][i] > a[i+1][i+1])
         {
